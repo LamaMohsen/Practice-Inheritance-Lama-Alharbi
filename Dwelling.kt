f378cc313d78c660fce84a4862f203088be82116
LamaMohsen <109925309+LamaMohsen@users.noreply.github.com>
2022-08-07 12:38:18 +0300

    Create Dwelling.kt

---
+++ Dwelling.kt
@@ -0,0 +1,52 @@
+fun main() {
+    //create SeqaureCabin
+    val sqaureCabin= SeqaureCabin(10)
+    
+    with(sqaureCabin){
+    println("capcity:${capacity}")
+    println("metrail:${buildingMaterial}")
+     println("Has Room??:${hasroom()}") }
+    val roundhut=RoundHut(1)
+    with(roundhut){
+        	println("\nRound Hut\n============")
+    		println("Material: ${buildingMaterial}")
+    		println("Capacity: ${capacity}")   
+    		println("Has Room?? ${hasroom()}")
+    }
+      val roundTower = roundtoure(1)
+    
+    with(roundTower){
+        	println("\nRound Tower\n============")
+    		println("Material: ${buildingMaterial}")
+    		println("Capacity: ${capacity}")   
+    		println("Has Room?? ${hasroom()}")
+    }
+}
+
+//super class
+abstract class Dwelling( private var residents:Int){//Bulding class
+    //attributes
+     abstract val buildingMaterial:String
+     abstract val capacity:Int
+    
+    //functions-method
+    fun hasroom():Boolean{
+        return residents<=capacity
+    }
+} 
+
+// subclass
+class SeqaureCabin (residents:Int):Dwelling(residents){
+    override val buildingMaterial ="wood"
+    override val capacity=6
+} 
+
+open class RoundHut(residents:Int) :Dwelling(residents){
+    override val buildingMaterial ="straw"
+    override val capacity=4
+    
+}
+class roundtoure (residents:Int):RoundHut(residents){
+     override val buildingMaterial ="stone"
+    override val capacity=3
+}
